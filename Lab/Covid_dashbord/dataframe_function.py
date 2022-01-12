f6698b6fffe09dba5603558dab43cd89c6fa7d3d
AmalDerbali <89392991+AmalDerbali@users.noreply.github.com>
2022-01-12 11:41:08 +0100

    try add function

---
+++ Lab/Covid_dashbord/dataframe_function.py
@@ -0,0 +1,20 @@
+
+#function based on lektion
+
+import pandas as pd
+
+class StockData:
+    """Class method to get and process local stock data"""
+    def __init__(self, data_path: str = "C:/Users/Amal Derbali/Documents/GitHub/Databehandling_AmalDerbali/Lab/Covid_dashbord/") -> None:
+        self._data_path = data_path
+
+def stock_dataframe(self, stockname: str) ->list:
+    stock_df = []
+    for path_ending in ["Covid19_vecka.xlsx"]:
+        path = self._data_path+stockname+path_ending
+        stock = pd.read_excel(path, index_col = 0, parse_date = True)
+        stock.index.rename("Vecka", inplace=True)
+        stock_df.append(stock)
+    
+    return stock_df
+        
