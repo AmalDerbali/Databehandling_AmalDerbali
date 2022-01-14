489d3681fd9a6158acd266121453107baac52c33
AmalDerbali <89392991+AmalDerbali@users.noreply.github.com>
2022-01-15 00:37:10 +0100

    change data

--- Lab/Covid_dashbord/dataframe_function.py
+++ Lab/Covid_dashbord/dataframe_function.py
@@ -2,19 +2,22 @@
 #function based on lektion
 
 import pandas as pd
+
 df= pd.read_excel("C:/Users/Amal Derbali/Documents/GitHub/Databehandling_AmalDerbali/Lab/Covid_dashbord/Covid19_vecka.xlsx")
+print(df.head())
+
 class StockData:
     """Class method to get and process local stock data"""
-    def __init__(self, data_path: str = "C:/Users/Amal Derbali/Documents/GitHub/Databehandling_AmalDerbali/Lab/Covid_dashbord/") -> None:
-        self._data_path = data_path
-
-def stock_dataframe(self, stockname: str) ->list:
-    stock_df = []
-    for path_ending in ["Covid19_vecka.xlsx"]:
-        path = self._data_path+stockname+path_ending
-        stock = pd.read_excel(path, index_col = 0, parse_date = True)
-        stock.index.rename("Vecka", inplace=True)
-        stock_df.append(stock)
+    def __init__(self, data_folder: str = "C:/Users/Amal Derbali/Documents/GitHub/Databehandling_AmalDerbali/Lab/Covid_dashbord/") -> None:
+        self._data_folder = data_folder
     
-    return stock_df
+    def stock_dataframe(self, stockname: str) ->list:
+        stock_df = []
+        for path_ending in ["Covid19_vecka.xlsx"]:
+            path = self._data_folder+stockname+path_ending
+            stock = pd.read_excel(path, index_col = 0, parse_date = True)
+            stock.index.rename("Vecka", inplace=True)
+            stock_df.append(stock)
+        
+        return stock_df
         
